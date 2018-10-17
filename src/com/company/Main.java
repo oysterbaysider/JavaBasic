2259f014a6d1b867fe67d7cd07911de461f5e804
Ting Li <oysterbaysider@gmail.com>
2018-10-17 12:07:47 +1100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Bloody Hell, I haven't touch Java for such a long time!");
+    }
+}
